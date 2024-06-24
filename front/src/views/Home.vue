c93268e36d6b856cf26bd95a1c6994689456f632
kimyongmin1@naver.com <kimyongmin1@naver.com>
2024-06-24 22:12:05 +0900

    home file change

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   
   <v-card class="mx-auto" elevation="5">
-    <v-card-title>2024 겨울 YOUTHVISION CAMP</v-card-title>
-    <v-card-subtitle>12 120 3000 "하나님 나라를 세우라"</v-card-subtitle>
+    <v-card-title>2024 여름 YOUTHVISION CAMP</v-card-title>
+    <v-card-subtitle>12 120 3000 "다음세대들이여 성령의 바람을 일으켜라 24.8.6~8"</v-card-subtitle>
     <div id="area">
       <iframe
       id="video"
       width="100%"
       height="100%"
-      src="https://www.youtube.com/embed/KFpYMopxueo?si=7SqRUX3eg933Y_aL"
+      src="https://www.youtube.com/embed/x9lEnrKqxPE?si=LPYXPIcUc_CyGi4o"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -30,34 +30,11 @@ import { computed, onMounted, ref } from "vue";
 import { useStore } from "vuex";
 
 const testText = ref("testText");
-const store = useStore();
-const currentUserList = computed(
-  () => store.getters["socketStore/currentUserList"]
-);
-// axios.get("/api/public/upload/9db4e216aea5e6db34aef57e26ea9cce")
-// .then(({ data }) => {
-//     console.log(data.msg);
-// });
-// const boardlist = ref(null);
-// onMounted(() => {
-//   // axios.get("/api/public/testText").then(({ data }) => {
-//   //   console.log(data.msg);
-//   //   testText.value = data.msg;
-//   // });
-//   const io = new IntersectionObserver(
-//     (entries) => {
-//       entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//           entry.target.classList.add("tada");
-//         } else {
-//           entry.target.classList.remove("tada");
-//         }
-//       });
-//     },
-//     { threshold: [0.3] }
-//   );
-//   io.observe(boardlist.value.$el);
-// });
+
+axios.get("/api/user/youtube/live")
+ .then(({ data }) => {
+     console.log(data);
+});
 </script>
 
 <style>
