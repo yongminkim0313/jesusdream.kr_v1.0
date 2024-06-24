1f276b248817694bf359bb5e9ff72fb46fce4690
kimyongmin <kimyongmin1@naver.com>
2024-06-24 23:18:42 +0900

    modified:   front/src/views/Home.vue

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,11 +2,7 @@
   
   <v-card class="mx-auto" elevation="5">
     <v-card-title>2024 여름 YOUTHVISION CAMP</v-card-title>
-<<<<<<< HEAD
-    <v-card-subtitle>12 120 3000 "다음세대들이여 성령의 바람을 일으켜라 24.8.6~8"</v-card-subtitle>
-=======
     <v-card-subtitle>"다음세대들이여 성령의 바람을 일으켜라 24.8.6~8"</v-card-subtitle>
->>>>>>> 01170983b5d26ea98adcb667aeb5bc71d1f4b8b3
     <div id="area">
       <iframe
       id="video"
@@ -34,11 +30,34 @@ import { computed, onMounted, ref } from "vue";
 import { useStore } from "vuex";
 
 const testText = ref("testText");
-
-axios.get("/api/user/youtube/live")
- .then(({ data }) => {
-     console.log(data);
-});
+const store = useStore();
+const currentUserList = computed(
+  () => store.getters["socketStore/currentUserList"]
+);
+// axios.get("/api/public/upload/9db4e216aea5e6db34aef57e26ea9cce")
+// .then(({ data }) => {
+//     console.log(data.msg);
+// });
+// const boardlist = ref(null);
+// onMounted(() => {
+//   // axios.get("/api/public/testText").then(({ data }) => {
+//   //   console.log(data.msg);
+//   //   testText.value = data.msg;
+//   // });
+//   const io = new IntersectionObserver(
+//     (entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           entry.target.classList.add("tada");
+//         } else {
+//           entry.target.classList.remove("tada");
+//         }
+//       });
+//     },
+//     { threshold: [0.3] }
+//   );
+//   io.observe(boardlist.value.$el);
+// });
 </script>
 
 <style>
