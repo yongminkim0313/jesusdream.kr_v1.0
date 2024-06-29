c75cc1416a15d1c08f86e52950d9efb989c436dc
kimyongmin <kimyongmin1@naver.com>
2024-06-29 23:07:37 +0900

    modified:   front/src/layouts/default/AppBar.vue 	modified:   front/src/main.js

--- front/src/layouts/default/AppBar.vue
+++ front/src/layouts/default/AppBar.vue
@@ -3,8 +3,6 @@
     <v-spacer></v-spacer>  
       <v-app-bar-title @click="goHome" class="text-center text-overline" style="cursor: pointer;"> YOUTHVISION </v-app-bar-title>
     <v-spacer></v-spacer>  
-{{firebaseData}}
-{{connect}}
     <template v-slot:prepend> </template>
     <template v-slot:append>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
@@ -201,12 +199,13 @@ export default {
   name: "App",
   components: {},
   data() {
-    return { drawer: null , firebaseData: {data:'init'}, connect: ''};
+    return { drawer: null , connect: ''};
   },
   created() {
-	this.$writeData('test',{no: 1, name:'테스트입니다.'});
-	this.$readData('test',(val)=>{this.firebaseData = val;});
+	//this.$writeData('test',{no: 1, name:'테스트입니다.'});
+	//this.$readData('test',(val)=>{this.firebaseData = val;});
 	this.$onFirebase((val)=>{this.connect = val;});
+  this.$authFirebase((user)=>{console.log(user);});
   },
   watch: {
     $route: function (to, from) {
--- front/src/main.js
+++ front/src/main.js
@@ -46,24 +46,45 @@ app.config.globalProperties.$filters = {
     }
 }
 
+function printDate(p){
+  // 오늘 날짜
+  const today = new Date(); // 년도
+  const year = today.getFullYear(); // 월
+  const month = (today.getMonth() + 1).toString().padStart(2, '0');  // 일
+  const day = today.getDate().toString().padStart(2, '0'); // yyyymmdd
+  const hour = today.getHours().toString().padStart(2, '0');
+  const minute = today.getMinutes().toString().padStart(2, '0');
+  const second = today.getSeconds().toString().padStart(2, '0');
+  if(p == 1){
+    return `${year}-${month}-${day}`;
+  }else{
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+  }
+}
+
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, set, onValue } from "firebase/database";
-import { getAuth } from "firebase/auth";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
+  apiKey: "AIzaSyANmSg8h-zgpDQtiFtxlzJugpDloX8FjLs",
+  authDomain: "jesusdream-3fd59.firebaseapp.com",
   databaseURL: "https://jesusdream-3fd59-default-rtdb.firebaseio.com",
+  projectId: "jesusdream-3fd59",
+  storageBucket: "jesusdream-3fd59.appspot.com",
+  messagingSenderId: "722215627190",
+  appId: "1:722215627190:web:6f7047ada83c852d76b917",
+  measurementId: "G-61V3WXQJDP"
 };
 
 // Initialize Firebase
 const fireapp = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(fireapp);
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(fireapp);
-
 
 function writeData(key, val) {
   const db = getDatabase();
@@ -71,7 +92,6 @@ function writeData(key, val) {
 }
 
 app.config.globalProperties.$writeData = writeData;
-
 
 function readData(key, callback) {
 	const db = getDatabase();
@@ -89,19 +109,58 @@ function onFirebase (callback) {
   onValue(connectedRef, (snap) => {
     if (snap.val() === true) {
       console.log("connected");
-		callback("connected");
+		  callback("connected");
     } else {
       console.log("not connected");
-		callback("disconnected");
+		  callback("disconnected");
     }
   });
 }
 
 app.config.globalProperties.$onFirebase = onFirebase;
 
-//if(!app.$cookies.get('tmpr_cookie')) app.$cookies.set('tmpr_cookie',v4(),60 * 60 * 1, null, null, null, 'Strict');
-//if(!app.$cookies.get('prmanent_cookie')) app.$cookies.set('prmanent_cookie',v4(), 60 * 60 * 24 * 365, null, null, null, 'Strict');
+//console.log("app.$cookies.get('jesusdream_tmpr_cookie')", app.$cookies.get('jesusdream_tmpr_cookie'));
+if(!app.$cookies.get('jesusdream_tmpr_cookie')){
+  var uuid = v4();
+  var yyyymmdd = printDate(1);
+  app.$cookies.set('jesusdream_tmpr_cookie', uuid, 60 * 60 * 1, null, null, null, 'Strict');
+  writeData(`tmprVisit/${yyyymmdd}/${uuid}`, printDate());
+} 
+if(!app.$cookies.get('jesusdream_prmanent_cookie')){
+  var uuid = v4();
+  var yyyymmdd = printDate(1);
+  app.$cookies.set('jesusdream_prmanent_cookie',uuid, 60 * 60 * 24 * 365, null, null, null, 'Strict');
+  writeData(`prmanentVisit/${uuid}`, yyyymmdd);
+} 
   
+function authFirebase(callback){
+  const auth = getAuth();
+  signInAnonymously(auth)
+    .then(() => {
+      // Signed in..
+      console.log("Signed in");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(errorCode, errorMessage);
+      // ...
+    });
+  
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      //const uid = user.uid;
+      callback(user);
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
+}
+
+app.config.globalProperties.$authFirebase = authFirebase;
 
 //import { socketStore } from '@/modules/socketStore';
 //import { mapStore } from '@/modules/mapStore';
